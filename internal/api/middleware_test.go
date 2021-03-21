aec60c31670e532ea982c3d2f50db6b7b312f265
Lukas Gruber <lgruber.mmt-m2019@fh-salzburg.ac.at>
2021-03-21 16:02:37 +0100

    refactor: combine multiple suffixed to one function

--- internal/api/middleware_test.go
+++ internal/api/middleware_test.go
@@ -13,82 +13,84 @@ import (
 	"testing"
 )
 
-func TestAuthRequired_returns_401_if_no_auth_is_provided(t *testing.T) {
-	store := memstore.NewStore([]byte("secret"))
-
-	r := gin.Default()
-	r.Use(sessions.Sessions("session", store))
-	r.Use(AuthRequired)
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/", nil)
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, w.Code, http.StatusUnauthorized)
-}
-
 func TestAuthRequired(t *testing.T) {
-	rr := httptest.NewRecorder()
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	t.Run("allows access if request is authenticated", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
 
-	r := setUp()
-	r.Use(func(c *gin.Context) {
-		session := sessions.Default(c)
-		session.Set("userEmail", "test@test.com")
-		session.Save()
-	})
-	r.Use(AuthRequired)
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "ok")
-	})
-	r.ServeHTTP(rr, request)
+		r := setUp()
+		r.Use(func(c *gin.Context) {
+			session := sessions.Default(c)
+			session.Set("userEmail", "test@test.com")
+			session.Save()
+		})
+		r.Use(AuthRequired)
+		r.GET("/", func(c *gin.Context) {
+			c.String(http.StatusOK, "ok")
+		})
+		r.ServeHTTP(rr, request)
 
-	assert.Equal(t, http.StatusOK, rr.Code)
+		assert.Equal(t, http.StatusOK, rr.Code)
+	})
+
+	t.Run("prevents access to unauthorized requests", func(t *testing.T) {
+		r := gin.Default()
+		r.Use(sessions.Sessions("session", memstore.NewStore([]byte("secret"))))
+		r.Use(AuthRequired)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, w.Code, http.StatusUnauthorized)
+	})
 }
 
 func TestErrorHandle(t *testing.T) {
-	rr := httptest.NewRecorder()
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	t.Run("renders custom error template", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
 
-	r := gin.Default()
-	r.Use(ErrorHandle)
-	r.GET("/", func(c *gin.Context) {
-		c.Status(http.StatusTeapot)
-		c.Error(errors.New("foobar"))
+		r := gin.Default()
+		r.Use(ErrorHandle)
+		r.GET("/", func(c *gin.Context) {
+			c.Status(http.StatusTeapot)
+			c.Error(errors.New("foobar"))
+		})
+		r.ServeHTTP(rr, request)
+
+		assert.Equal(t, http.StatusTeapot, rr.Code, "does not override an already set response status")
+		body, _ := io.ReadAll(rr.Body)
+		assert.Contains(t, string(body), strconv.Itoa(http.StatusTeapot), "should contain response status somewhere in the template")
 	})
-	r.ServeHTTP(rr, request)
 
-	assert.Equal(t, http.StatusTeapot, rr.Code, "does not override an already set response status")
-	body, _ := io.ReadAll(rr.Body)
-	assert.Contains(t, string(body), strconv.Itoa(http.StatusTeapot), "should contain response status somewhere in the template")
-}
+	t.Run("falls back to internal error if no error was set prior", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
 
-func TestErrorHandle_set_an_error_code(t *testing.T) {
-	rr := httptest.NewRecorder()
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		r := gin.Default()
+		r.Use(ErrorHandle)
+		r.GET("/", func(c *gin.Context) {
+			c.Error(errors.New("foobar"))
+		})
+		r.ServeHTTP(rr, request)
 
-	r := gin.Default()
-	r.Use(ErrorHandle)
-	r.GET("/", func(c *gin.Context) {
-		c.Error(errors.New("foobar"))
+		assert.Equal(t, http.StatusInternalServerError, rr.Code, "should fallback to 500 status if no status was set prior")
 	})
-	r.ServeHTTP(rr, request)
 
-	assert.Equal(t, http.StatusInternalServerError, rr.Code, "should fallback to 500 status if no status was set prior")
-}
+	t.Run("Does nothing if no error occurred", func(t *testing.T) {
+		rr := httptest.NewRecorder()
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
 
-func TestErrorHandle_does_nothing_if_no_error_occured(t *testing.T) {
-	rr := httptest.NewRecorder()
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		r := gin.Default()
+		r.Use(ErrorHandle)
+		r.GET("/", func(c *gin.Context) {
+			c.String(http.StatusCreated, "Hello world")
+		})
+		r.ServeHTTP(rr, request)
 
-	r := gin.Default()
-	r.Use(ErrorHandle)
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusCreated, "Hello world")
+		assert.Equal(t, http.StatusCreated, rr.Code)
+		body, _ := io.ReadAll(rr.Body)
+		assert.Equal(t, string(body), "Hello world")
 	})
-	r.ServeHTTP(rr, request)
-
-	assert.Equal(t, http.StatusCreated, rr.Code)
-	body, _ := io.ReadAll(rr.Body)
-	assert.Equal(t, string(body), "Hello world")
 }
--- internal/api/oauth_test.go
+++ internal/api/oauth_test.go
@@ -11,17 +11,19 @@ import (
 	"testing"
 )
 
-func TestGetEnv_returns_value_of_env_var(t *testing.T) {
-	key := "foo"
-	value := "bar"
-	_ = os.Setenv(key, value)
-	defer os.Unsetenv(key)
+func TestGetEnv(t *testing.T) {
+	t.Run("returns value of env var", func(t *testing.T) {
+		key := "foo"
+		value := "bar"
+		_ = os.Setenv(key, value)
+		defer os.Unsetenv(key)
 
-	assert.Equal(t, getEnv(key, ""), value)
-}
+		assert.Equal(t, getEnv(key, ""), value)
+	})
 
-func TestGetEnv_returns_fallback_value_if_env_var_is_missing(t *testing.T) {
-	assert.Equal(t, getEnv("foo", "fallback"), "fallback")
+	t.Run("returns fallback value if env var is missing", func(t *testing.T) {
+		assert.Equal(t, getEnv("foo", "fallback"), "fallback")
+	})
 }
 
 func TestLoginHandler(t *testing.T) {
