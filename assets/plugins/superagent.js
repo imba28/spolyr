10f375cedf706643399e919cbaa84316fc027e07
Lukas Gruber <lgruber.mmt-m2019@fh-salzburg.ac.at>
2022-07-10 17:27:25 +0200

    fix(superagent): do not repeat requests if refreshing the token still produces a 401 error

--- assets/plugins/superagent.js
+++ assets/plugins/superagent.js
@@ -3,14 +3,6 @@ import {AuthApi} from '@/openapi';
 const authApi = new AuthApi();
 
 /**
- *
- * @param{Request} request
- */
-function reset(request) {
-  request._endCalled = false;
-}
-
-/**
  * This plugin intercepts http responses.
  * If a requests triggers a 401 response, two things happen:
  *   1. The plugin tries to refresh the JWT
@@ -23,17 +15,18 @@ function refreshTokenMiddleware(Request) {
 
   Request.end = async (callback) => {
     originalEnd.call(Request, async function(err, response) {
-      if (!err || err.status !== 401) {
+      if (Request.__proto__._refreshed || !err || err.status !== 401) {
+        Request.__proto__._refreshed = false;
         callback(err, response);
         return;
       }
+
+      Request.__proto__._refreshed = true;
 
       try {
         // acquire fresh access token...
         await authApi.authRefreshGet();
 
-        // repeat the original request
-        reset(Request);
         originalEnd.call(Request, callback);
       } catch (e) {
         callback(new Error('could not refresh token'), response);
